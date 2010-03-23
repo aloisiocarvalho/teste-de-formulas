93a941ce5da84b23a67838e66cd1fc69a4420510
aloisio <aloisio@elianapedrosa.com.br>
2010-03-22 21:59:57 -0300

    calculo com include

---
+++ calc.php
@@ -0,0 +1,21 @@
+<?php
+
+	function somar($num1 , $num2) {
+	return $num1 + $num2;
+	}
+	
+	function subtrair ($num1 , $num2) {
+	return $num1 - $num2;
+	}
+	
+	function multiplicar ($num1 , $num2) {
+	return $num1 * $num2;
+	}
+	
+	function dividir ($num1 , $num2) {
+	return $num1 / $num2;
+	}
+	
+
+?>
+
---
+++ calc_test.php
@@ -0,0 +1,10 @@
+<pre><?php
+
+	include 'calc.php';
+
+	echo '5 + 2 =' . somar (5,2) . '<br>'; 
+	echo '5 - 2 =' . subtrair (5,2) . '<br>';
+	echo '5 * 2 =' . multiplicar (5,2) . '<br>';
+	echo '5 / 2 =' . dividir (5,2) . '<br>';
+
+?></pre>
