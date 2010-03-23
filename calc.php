8364208c7630640f27e176a92e3b56663bdc7d99
aloisio <aloisio@elianapedrosa.com.br>
2010-03-22 22:16:00 -0300

    novos calculos

--- calc.php
+++ calc.php
@@ -1,19 +1,19 @@
 <?php
 
 	function somar($num1 , $num2) {
-	return $num1 + $num2;
+		return $num1 + $num2;
 	}
 	
 	function subtrair ($num1 , $num2) {
-	return $num1 - $num2;
+		return $num1 - $num2;
 	}
 	
 	function multiplicar ($num1 , $num2) {
-	return $num1 * $num2;
+		return $num1 * $num2;
 	}
 	
 	function dividir ($num1 , $num2) {
-	return $num1 / $num2;
+		return $num1 / $num2;
 	}
 	
 
--- calc_test.php
+++ calc_test.php
@@ -2,9 +2,12 @@
 
 	include 'calc.php';
 
-	echo '5 + 2 =' . somar (5,2) . '<br>'; 
-	echo '5 - 2 =' . subtrair (5,2) . '<br>';
-	echo '5 * 2 =' . multiplicar (5,2) . '<br>';
-	echo '5 / 2 =' . dividir (5,2) . '<br>';
-
+	echo '5 + 2 = ' . somar (5,2)			 . '<br>'; 
+	echo '5 - 2 = ' . subtrair (5,2)			 . '<br>';
+	echo '5 * 2 = ' . multiplicar (5,2)		 . '<br>';
+	echo '5 / 2 = ' . dividir (5,2)			 . '<br>';
+	
+	echo '5 + 2 * 3 = ' . somar (5, multiplicar (2,3)) . '<br>';
+	echo '8 - 3 + 5 = ' . somar (5, subtrair(8,3), 5) . '<br>';
+	echo '9 / 3 + 4 * 2 = ' . somar (dividir (9, 3), multiplicar (4, 2)) . '<br>';
 ?></pre>
